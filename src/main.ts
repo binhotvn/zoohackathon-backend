b96ae47f213a9143836902b61c1688d17a00c67b
Thanh Binh (shanky) <72479657+bshankya93@users.noreply.github.com>
2021-11-15 02:35:30 +0700

    ssl add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as fs from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3001);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.wildgogh.altwis.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.wildgogh.altwis.com/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  await app.listen(443);
 }
 bootstrap();
